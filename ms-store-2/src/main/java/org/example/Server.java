f27d57feef860c0527f933a160d187ac70749b54
rafael <rafaelmagalhaes1@live.com>
2024-06-19 11:58:58 -0300

    feat: microservice Store2 created

--- ms-store-2/src/main/java/org/example/Server.java
+++ ms-store-2/src/main/java/org/example/Server.java
@@ -6,13 +6,13 @@ import java.net.InetSocketAddress;
 import java.util.concurrent.Executors;
 
 public class Server {
-  private static final int PORT = 8081;
+  private static final int PORT = 8082;
   private HttpServer server;
 
   public Server() {
     try {
       server = HttpServer.create(new InetSocketAddress(PORT), 0);
-      server.createContext("/store1/sales", new Service());
+      server.createContext("/store2/sales", new Service());
       server.setExecutor(Executors.newFixedThreadPool(10));
     } catch (IOException e) {
       System.out.print("\nServer creation failed: " + e.getMessage());
@@ -21,6 +21,6 @@ public class Server {
 
   public void start() {
     server.start();
-    System.out.print("\nStore 1 Service started at the PORT " + PORT);
+    System.out.print("\nStore 2 Service started at the PORT " + PORT);
   }
 }
